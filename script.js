c10c7768c052148209d246891f466366e7204773
T-Khoury <120677926+T-Khoury@users.noreply.github.com>
2023-03-09 09:15:13 -0800

    Add book-placement logic

--- script.js
+++ script.js
@@ -9,30 +9,65 @@ const pageCount = document.getElementById('page-count');
 const readStatus = document.getElementById('read-status');
 const submitButton = document.getElementById('submit-button');
 
+const shelves = document.querySelectorAll('.shelf');
+
+
+
 const myLibrary = [];
+const bookIcons = ["images/book1.png", "images/book2.png", "images/book3.png", "images/book4.png", "images/book5.png", "images/book6.png", "images/book7.png"];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    let readYet;
-    if (this.read == true) {
-      readYet = "completed";
-    } else {
-      readYet = "not yet completed";
+  this.isPlaced = false;
+}
+
+function assignIcons() {
+  myLibrary.forEach(book => {
+    if (!book.icon) {
+      const randomNum = Math.floor(Math.random() * bookIcons.length);
+      book.icon = bookIcons[randomNum];
     }
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readYet}.`;
-  };
+  })
 }
 
 function addBookToLibrary() {
-  
   const newBook = new Book(bookTitle.value, bookAuthor.value, pageCount.value, readStatus.value);
   myLibrary.push(newBook);
-
 }
+
+function shelfPlacement(book) {
+  for (let i = 0; i < shelves.length; i++) {
+    if (shelves[i].childElementCount < 3) {
+      shelves[i].appendChild(book);
+      return
+    }
+  }
+}
+
+
+
+function createBookElements() {
+  myLibrary.forEach(book => {
+    if (book.isPlaced === false) {
+    const bookElement = document.createElement('img');
+    bookElement.src = book.icon;
+    bookElement.classList.add('book');
+    shelfPlacement(bookElement)
+    book.isPlaced = true;
+    }
+  })
+}
+
+function displayBooks() {
+  myLibrary.forEach(() => {
+    
+  })
+}
+
+
 
 modalBtn.onclick = function() {
   modal.style.display = "block"
@@ -51,6 +86,8 @@ window.onclick = function(e) {
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
   addBookToLibrary();
+  assignIcons();
+  createBookElements();
   modal.style.display = "none"
   bookCreate.reset();
 })
